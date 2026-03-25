3545b135b36320c5ffaeb985129a42b50574bc12
Sean Culligan <268052258+sean-culligan@users.noreply.github.com>
2026-03-24 23:00:40 -0400

    Update fmr to v0.0.7

--- Formula/fmr.rb
+++ Formula/fmr.rb
@@ -1,8 +1,8 @@
 class Fmr < Formula
   desc "Frontmatter Replacment"
   homepage "https://github.com/watercorp/fmr"
-  url "https://github.com/watercorp/fmr/archive/refs/tags/v0.0.4.tar.gz"
-  sha256 "353a5625fb1240feb365e578962311317787ce86618b3734301aa0fa7fd94ad3"
+  url "https://github.com/watercorp/fmr/archive/refs/tags/v0.0.7.tar.gz"
+  sha256 "82e88844a2222e93ed6e3ebb4f468d0d496057b0d39f7642d5746e3ce2a89217"
   license "MIT"
 
   depends_on "go" => :build
